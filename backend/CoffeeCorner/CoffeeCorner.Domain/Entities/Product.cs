64301d80e5b7a1136999b8d53e1a402e90b45f24
Tytus Harlender <t.harlender@godeltech.com>
2025-10-21 18:07:33 +0200

    [phase01] configuring DB connection; adding initial migration with products table; adding data seeding

--- backend/CoffeeCorner/CoffeeCorner.Domain/Entities/Product.cs
+++ backend/CoffeeCorner/CoffeeCorner.Domain/Entities/Product.cs
@@ -5,6 +5,6 @@ public class Product
     public int Id { get; set; }
     public Guid PublicId { get; set; }
     public string Name { get; set; } = string.Empty;
-    public double Price { get; set; }
+    public decimal Price { get; set; }
     public string Description { get; set; } = string.Empty;
 }
